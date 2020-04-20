61bbe09c69de9865e2d5f2aee64c4af185a4f9b9
Ari Mendelow <ari@mendelow.me>
2020-04-20 16:13:06 -0700

    Add comment

--- app/graphql/mutations/user/logout.rb
+++ app/graphql/mutations/user/logout.rb
@@ -6,6 +6,10 @@ class Mutations::User::Logout < GraphQL::Schema::Mutation
 
   def resolve
     if context[:current_user]
+      # When a token is dispatched for a user, the jti claim is taken from the jti column in the model (which has been initialized when the record has been created).
+      # At every authenticated action, the incoming token jti claim is matched against the jti column for that user. The authentication only succeeds if they are the same.
+      # When the user requests to sign out its jti column changes, so that provided token won't be valid anymore.
+      # That's what's happening here - we're changing the JTI column
       context[:current_user].update(jti: SecureRandom.uuid)
       return true
     end 
