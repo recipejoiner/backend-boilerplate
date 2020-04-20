76e1cc3452dd37b4ec480dd2220beff33dd69f4d
Ari Mendelow <ari@mendelow.me>
2020-04-20 13:35:02 -0700

    Move line

--- db/schema.rb
+++ db/schema.rb
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 2020_01_05_201236) do
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
