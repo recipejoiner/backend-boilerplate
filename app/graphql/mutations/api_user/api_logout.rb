1dc1a4d67d12863955d15759ba6ddc674fcd00ab
Ari Mendelow <ari@mendelow.me>
2020-04-22 13:40:20 -0700

    Edit description

--- app/graphql/mutations/api_user/api_logout.rb
+++ app/graphql/mutations/api_user/api_logout.rb
@@ -1,7 +1,7 @@
 class Mutations::ApiUser::ApiLogout < GraphQL::Schema::Mutation
 
   null true
-  description "Logout for users"
+  description "Logout for API users"
   payload_type Boolean
 
   def resolve
@@ -10,7 +10,7 @@ class Mutations::ApiUser::ApiLogout < GraphQL::Schema::Mutation
       # At every authenticated action, the incoming token jti claim is matched against the jti column for that user. The authentication only succeeds if they are the same.
       # When the user requests to sign out its jti column changes, so that provided token won't be valid anymore.
       # That's what's happening here - we're changing the JTI column
-      context[:current_api_user].update!(jti: SecureRandom.uuid)
+      context[:current_api_user].update(jti: SecureRandom.uuid)
       return true
     end 
     false
