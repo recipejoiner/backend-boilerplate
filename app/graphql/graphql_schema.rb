d49ee3587b9bd2f6920762bff4ed13ccc0d279f5
Ari Mendelow <ari@mendelow.me>
2020-04-23 12:19:12 -0700

    Add pagination plugin

--- app/graphql/graphql_schema.rb
+++ app/graphql/graphql_schema.rb
@@ -1,4 +1,8 @@
 class GraphqlSchema < GraphQL::Schema
+    # Add the plugin for connection pagination
+    use GraphQL::Pagination::Connections
+
+    # base types
     query(Types::QueryType)
     mutation(Types::MutationType)
 
