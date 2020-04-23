32fc5fd8b8942ce2a42c8e5215a21a38512cb4f7
Ari Mendelow <ari@mendelow.me>
2020-04-23 13:30:18 -0700

    Add max page size in pagination

--- app/graphql/graphql_schema.rb
+++ app/graphql/graphql_schema.rb
@@ -1,6 +1,7 @@
 class GraphqlSchema < GraphQL::Schema
     # Add the plugin for connection pagination
     use GraphQL::Pagination::Connections
+    default_max_page_size 50
 
     # base types
     query(Types::QueryType)
