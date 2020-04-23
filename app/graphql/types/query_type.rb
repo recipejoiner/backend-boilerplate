8fc4db00797b73d641e3cacc9bb22a6306cc987a
Ari Mendelow <ari@mendelow.me>
2020-04-23 13:31:26 -0700

    Add pagination to the all_users query. Yes, kids, it's that easy.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,10 +2,9 @@ module Types
   class QueryType < BaseObject
     # For getting info about the current user
     field :me, resolver: Resolvers::Me
-    field :all_users, [UserType], null: false, resolver: Resolvers::AllUsers
+    field :all_users, Types::UserType.connection_type, null: false, resolver: Resolvers::AllUsers
     field :user_by_id, UserType, null: true, resolver: Resolvers::UserByID do
       argument :id, ID, required: true
     end
   end
 end
-  
