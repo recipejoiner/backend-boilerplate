17334ebb2a34a5cba2129300ba6d196169dfdd15
Ari Mendelow <ari@mendelow.me>
2020-04-23 11:11:36 -0700

    Add sample seed comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
 User.create(
   email: ENV['ADMIN_EMAIL'],
   password: ENV['ADMIN_PASSWORD'],
@@ -13,3 +21,4 @@ ApiUser.create(
   password: ENV['ADMIN_PASSWORD'],
   password_confirmation: ENV['ADMIN_PASSWORD'],
 )
+
