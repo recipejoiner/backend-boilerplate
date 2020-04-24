c40c215c2dae941847d4e6059594f6222abee217
Ari Mendelow <ari@mendelow.me>
2020-04-24 15:59:04 -0700

    Fix indentation

--- mini-node-server/server.js
+++ mini-node-server/server.js
@@ -9,14 +9,14 @@ const options = {
 https.createServer(options, function (req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html'
-});
-fs.readFile('./index.html', null, function (error, data) {
-    if (error) {
-        res.writeHead(404);
-        respone.write('Whoops! File not found!');
-    } else {
-        res.write(data);
-    }
-    res.end();
-});
+  });
+  fs.readFile('./index.html', null, function (error, data) {
+      if (error) {
+          res.writeHead(404);
+          response.write('Whoops! File not found!');
+      } else {
+          res.write(data);
+      }
+      res.end();
+  });
 }).listen(8000);
