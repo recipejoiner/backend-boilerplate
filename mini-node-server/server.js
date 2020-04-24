23134d040a3c41e897162a6929bacb3632c7ef40
Ari Mendelow <ari@mendelow.me>
2020-04-24 14:58:17 -0700

    SSL enabled for the mini node server using the same cert used by Rails

---
+++ mini-node-server/server.js
@@ -0,0 +1,22 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('../config/ssl/devwork.shmob4-key.pem'),
+  cert: fs.readFileSync('../config/ssl/devwork.shmob4.pem')
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+});
+fs.readFile('./index.html', null, function (error, data) {
+    if (error) {
+        res.writeHead(404);
+        respone.write('Whoops! File not found!');
+    } else {
+        res.write(data);
+    }
+    res.end();
+});
+}).listen(8000);
