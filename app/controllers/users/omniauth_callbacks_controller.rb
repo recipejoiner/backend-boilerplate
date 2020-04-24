1c2d48326129418a7ae3f76723c65a20c1de0937
Ari Mendelow <ari@mendelow.me>
2020-04-24 15:59:21 -0700

    Set token cookie on redirect

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include ActionController::Cookies
   def facebook
     if request.env["omniauth.auth"].info.email.blank?
       redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
@@ -9,8 +10,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      msg = {:token => @user.token}
-      render :json => msg
+      cookies[:token] = {
+        value: @user.token,
+        expires: 1.year,
+        domain: :all
+      }
+      if Rails.env.development?
+        redirect_to "https://devwork.shmob:8000/"
+      end
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       msg = {:error => "Error with logging in/signing up this user."}
